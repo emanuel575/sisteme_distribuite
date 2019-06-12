2c8cdec5c8da3d384c58e3eb8e2f19448d45f1f1
Emanuel <emanuelpetru92@gmail.com>
2019-06-12 22:45:54 +0300

    Evaluate expression works
    TO_DO: GUI

---
+++ Problema2_Calc_Expr/Problema2_Calc_Expr/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Data;
+
+
+namespace Problema2_Calc_Expr
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+            var input = "3+(4*3)";
+
+            DataTable dt = new DataTable();
+            var result = dt.Compute(input,"");
+
+            MessageBox.Show(result.ToString());
+        }
+    }
+}
