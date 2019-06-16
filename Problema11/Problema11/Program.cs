1adbe7d68647c785f5a806372e43fb296a7b3dc1
emanuel575 <emanuelpetru92@gmail.com>
2019-06-16 14:09:40 +0300

    works

--- Problema11/Problema11/Program.cs
+++ Problema11/Problema11/Program.cs
@@ -31,37 +31,58 @@ namespace Problema11
             sum += sumaS;
             m.ReleaseMutex();
         }
-        public static int[] getArr(int[] arr, int begin, int end)
+
+        public static void printArr(int[] arr)
         {
-            int[] res = new int[(end - begin) + 1];
-            for (int i = begin; i <= end; i++)
+            foreach(var x in arr)
             {
-                res[i] = arr[i];
+                Console.Write(x + " ");
             }
-            return res;
+            Console.WriteLine("spatiu");
+        }
+
+        public static int[] getSubArr(int[] arr, int begin, int end)
+        {
+            Mutex m = new Mutex();
+            m.WaitOne();
+            List<int> res = new List<int>();
+            for(int i=begin;i<=end;i++)
+            {
+                res.Add(arr[i]);
+            }
+            m.ReleaseMutex();
+            return res.ToArray();
         }
         static void Main(string[] args)
         {
-            int[] arr = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };
+            var arr = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };
+            List<int> asta = new List<int>(arr);
+
             int n = arr.Length;
-            Console.WriteLine("Length is {0}", n);
-            Thread[] threads = new Thread[5];
-            //threads[0] = new Thread(() => arrSum(getArr(arr, 0, (n / 4) - 1)));
-            //threads[1] = new Thread(() => arrSum(getArr(arr, (n / 4), (n / 2) - 1)));
-            //threads[2] = new Thread(() => arrSum(getArr(arr, (n / 2), ((3 * n) / 4) - 1)));
-            //threads[3] = new Thread(() => arrSum(getArr(arr, ((3 * n) / 4), n - 1)));
 
-            threads[0] = new Thread(() => arrSum(new List<int>(arr).GetRange(0, (n / 4) - 1).ToArray()));
-            threads[1] = new Thread(() => arrSum(new List<int>(arr).GetRange((n / 4), (n / 2) - 1).ToArray()));
-            //threads[2] = new Thread(() => arrSum(new List<int>(arr).GetRange((n / 2), ((3 * n) / 4) - 1).ToArray()));
-            //threads[3] = new Thread(() => arrSum(new List<int>(arr).GetRange(((3 * n) / 4), n - 1).ToArray()));
+            Thread[] threads = new Thread[4];
+            threads[0] = new Thread(() => arrSum(getSubArr(arr, 0, (n / 4) - 1)));
+            printArr(getSubArr(arr, 0, (n / 4) - 1));
+            threads[1] = new Thread(() => arrSum(getSubArr(arr, (n / 4), (n / 2) - 1)));
+            printArr(getSubArr(arr,(n / 4), (n / 2) - 1).ToArray());
+            threads[2] = new Thread(() => arrSum(getSubArr(arr, (n / 2), ((3 * n) / 4) - 1)));
+            printArr(getSubArr(arr,(n / 2), ((3 * n) / 4) - 1).ToArray());
+            threads[3] = new Thread(() => arrSum(getSubArr(arr, ((3 * n) / 4), n - 1)));
+            printArr(getSubArr(arr,((3 * n) / 4), n - 1).ToArray());
 
-            foreach (var t in threads)
+            try
             {
-                t.Start();
-                t.Join();
+                foreach (var t in threads)
+                {
+                    t.Start();
+                    t.Join();
+                }
             }
-
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.WriteLine("Suma finala : " + sum);
         }
     }
 }
