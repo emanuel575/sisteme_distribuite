7ef11fa4551b5826717d8bfc2b91319b5203795e
emanuel575 <emanuelpetru92@gmail.com>
2019-06-16 14:23:28 +0300

    problem 12 works, alpha can be read from keyboard

---
+++ Problema12/Problema12/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Problema12
+{
+    class Program
+    {
+        public static void multiply(int[] arr, int alpha)
+        {
+            int n = arr.Length;
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] *= alpha;
+            }
+        }
+
+        public static void sort(int[] arr)
+        {
+            Array.Sort(arr);
+        }
+
+        public static void print(int[] arr)
+        {
+            foreach (var el in arr)
+            {
+                Console.Write(el + " ");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Mutex m = new Mutex();
+            m.WaitOne();
+            int[] arr = new int[] { 2, 104, 1, 8762, 64531, 12, 375, 31531, 763, 23 };
+            m.ReleaseMutex();
+
+            Thread[] threads = new Thread[3];
+            threads[0] = new Thread(() => multiply(arr, 3));
+            threads[1] = new Thread(() => sort(arr));
+            threads[2] = new Thread(() => print(arr));
+
+            foreach(var t in threads)
+            {
+                t.Start();
+                t.Join();
+            }
+        }
+    }
+}
