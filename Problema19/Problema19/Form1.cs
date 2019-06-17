6ef58b67d1cbf2473c48b8de97860c45f09bb64d
emanuel575 <emanuelpetru92@gmail.com>
2019-06-17 15:47:18 +0300

    in working

---
+++ Problema19/Problema19/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Globalization;
+using System.Resources;
+
+namespace Problema19
+{
+    public partial class Form1 : Form
+    {
+        public ResourceManager res_man;
+        public CultureInfo cul;
+
+        public void show()
+        {
+            textBox1.Text = res_man.GetString("Hello", cul);
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
