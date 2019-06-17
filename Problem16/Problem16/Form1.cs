fb1a6c1a04ff6d65721f480f980e077d0efb9f25
emanuel575 <emanuelpetru92@gmail.com>
2019-06-17 14:56:12 +0300

    problem 16 works power bonbeu

---
+++ Problem16/Problem16/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Timers;
+using System.Windows.Forms;
+
+namespace Problem16
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            System.Threading.Timer myTimer = new System.Threading.Timer(new TimerCallback(showTime), null, 1000, 1000);
+
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public class SYSTEMTIME
+        {
+            public ushort wYear;
+            public ushort wMonth;
+            public ushort wDayOfWeek;
+            public ushort wDay;
+            public ushort wHour;
+            public ushort wMinute;
+            public ushort wSecond;
+            public ushort wMilliseconds;
+        }
+
+        [DllImport("kernel32.dll")]
+        public static extern void GetSystemTime(SYSTEMTIME time);
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+        public void showTime(object state)
+        {
+            var myTime = new SYSTEMTIME();
+            GetSystemTime(myTime);
+            DateTime dt = new DateTime(myTime.wYear, myTime.wMonth, myTime.wDay, myTime.wHour, myTime.wMinute, myTime.wSecond, myTime.wMilliseconds);
+            DateTime newDT = dt.ToLocalTime();
+            string currentTime = newDT.Hour.ToString() + " : " + newDT.Minute.ToString() + " : " + newDT.Second.ToString();
+            textBox1.Text = currentTime;
+        }
+    }
+}
--- Problema15/Problema15/Program.cs
+++ Problema15/Problema15/Program.cs
@@ -9,21 +9,36 @@ using System.IO;
 namespace Problema15
 {
     public delegate void read(string fileFromRead, string fileToWrite);
-
-
-
     class Program
     {
         static void readDelegate(string fileFromRead, string fileToWrite)
         {
             Mutex m = new Mutex();
             m.WaitOne();
+            //Console.WriteLine(File.ReadAllText(fileFromRead) + " -> " + Thread.CurrentThread.ManagedThreadId + "\n");
             File.AppendAllText(fileToWrite, File.ReadAllText(fileFromRead) + " -> " + Thread.CurrentThread.ManagedThreadId + "\n");
             m.ReleaseMutex();
         }
         static void Main(string[] args)
         {
+            Console.Write("Introduceti n = ");
+            var n = Int32.Parse(Console.ReadLine());
+
+            Mutex m = new Mutex();
+            m.WaitOne();
+            const string fileToWr = "result.txt";
+            m.ReleaseMutex();
+
+            for (int i = 0; i <= n; ++i)
+            {
+                File.WriteAllText(i.ToString(), (new Random().Next()).ToString());
+            }
+
             read r = new read(readDelegate);
+            for (int i = 0; i < n; i++)
+            {
+                var asyncRes = r.BeginInvoke(i.ToString(), fileToWr, null, null);
+            }
         }
     }
 }
