7467591def9727af1e94adddd93e9f401142b262
emanuel575 <emanuelpetru92@gmail.com>
2019-06-17 15:30:53 +0300

    problem 17 works

---
+++ Problema17/Problema17/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace Problema17
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.add = new System.Windows.Forms.Button();
+            this.toXML = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.CheckBoxes = true;
+            this.treeView1.Location = new System.Drawing.Point(12, 12);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(214, 426);
+            this.treeView1.TabIndex = 0;
+            // 
+            // add
+            // 
+            this.add.Location = new System.Drawing.Point(338, 12);
+            this.add.Name = "add";
+            this.add.Size = new System.Drawing.Size(75, 23);
+            this.add.TabIndex = 1;
+            this.add.Text = "add";
+            this.add.UseVisualStyleBackColor = true;
+            this.add.Click += new System.EventHandler(this.Add_Click);
+            // 
+            // toXML
+            // 
+            this.toXML.Location = new System.Drawing.Point(412, 190);
+            this.toXML.Name = "toXML";
+            this.toXML.Size = new System.Drawing.Size(135, 76);
+            this.toXML.TabIndex = 2;
+            this.toXML.Text = "toXML";
+            this.toXML.UseVisualStyleBackColor = true;
+            this.toXML.Click += new System.EventHandler(this.ToXML_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(232, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.toXML);
+            this.Controls.Add(this.add);
+            this.Controls.Add(this.treeView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Button add;
+        private System.Windows.Forms.Button toXML;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Problema17/Problema17/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace Problema17
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Add_Click(object sender, EventArgs e)
+        {
+            treeView1.Nodes.Add(textBox1.Text);
+        }
+
+        private void ToXML_Click(object sender, EventArgs e)
+        {
+            XmlWriter xmlWriter = XmlWriter.Create("res.xml");
+            xmlWriter.WriteStartDocument();
+            xmlWriter.WriteStartElement("treeView");
+            foreach (var node in treeView1.Nodes)
+            {
+                try
+                {
+                    xmlWriter.WriteStartElement("element");
+                    xmlWriter.WriteString(node.ToString());
+                    xmlWriter.WriteEndElement();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            xmlWriter.WriteEndDocument();
+            try
+            {
+                xmlWriter.Close();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
