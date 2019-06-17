da50f7e074d1431027387d5215d3f7169df2b58e
emanuel575 <emanuelpetru92@gmail.com>
2019-06-17 12:57:40 +0300

    problem 15 in working

---
+++ Problema15/Problema15/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.IO;
+
+namespace Problema15
+{
+    public delegate void read(string fileFromRead, string fileToWrite);
+
+
+
+    class Program
+    {
+        static void readDelegate(string fileFromRead, string fileToWrite)
+        {
+            Mutex m = new Mutex();
+            m.WaitOne();
+            File.AppendAllText(fileToWrite, File.ReadAllText(fileFromRead) + " -> " + Thread.CurrentThread.ManagedThreadId + "\n");
+            m.ReleaseMutex();
+        }
+        static void Main(string[] args)
+        {
+            read r = new read(readDelegate);
+        }
+    }
+}
